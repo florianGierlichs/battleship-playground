a010a873d19bf7fc1debcf9878899e3368de63b9
florianGierlichs <gierlichs@gmx.de>
2020-08-02 13:41:12 +0200

    Add basic css

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import './index.scss';
+
 const container = document.createElement('div');
+container.className = 'container';
 document.body.appendChild(container);
 container.innerHTML = 'Hallo Welt';
