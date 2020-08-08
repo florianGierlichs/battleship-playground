f20275a8311081034089e3397a619ffb37b445e3
florianGierlichs <gierlichs@gmx.de>
2020-08-08 14:05:24 +0200

    Add dynamic size board functionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 import './index.scss';
+import './boardSize';
+import boardSize from './boardSize';
+
+const size = 6;
+const board = boardSize(size);
+const { latitude, longitude } = board;
+console.log(latitude);
+console.log(longitude);
 
 const container = document.createElement('div');
 container.className = 'container';
 document.body.appendChild(container);
-container.innerHTML = 'Hallo Welt';
+
+longitude.forEach(() => {
+  const row = document.createElement('div');
+  container.className = 'row';
+  container.appendChild(row);
+  latitude.forEach(() => {
+    const square = document.createElement('div');
+    square.className = 'square';
+    row.appendChild(square);
+  });
+});
