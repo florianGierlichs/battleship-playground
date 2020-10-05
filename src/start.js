78637f980099360db18d88af9226ba8895d43fb7
florianGierlichs <gierlichs@gmx.de>
2020-10-05 17:40:37 +0200

    Add a headline

--- src/start.js
+++ src/start.js
@@ -1,15 +1,17 @@
 import createElement from './utils/createElement';
 
 export default function start() {
+  const headline = createElement('h1', 'headline', 'Battleship-Playground');
   const startContainer = createElement(
     'form',
     'startContainer',
-    'Please choose a size for your board'
+    'Please choose a size between 4 and 10 for your board'
   );
   const sizeInput = createElement('input', 'sizeInput');
   const startButton = createElement('button', 'startButton', 'Start');
 
   sizeInput.type = 'number';
+  document.body.appendChild(headline);
   document.body.appendChild(startContainer);
   startContainer.appendChild(sizeInput);
   startContainer.appendChild(startButton);
